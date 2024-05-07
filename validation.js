d47070bde0821969f268fc3c9ebb5056f0c8c840
Christopher Adams <christopher.m.adams2@gmail.com>
2024-05-07 13:00:43 -0700

    done

--- validation.js
+++ validation.js
@@ -2,23 +2,24 @@
 const submitButton = document.querySelector("#validate");
 
 submitButton.addEventListener("click", function (event) {
-    const userPassword = document.querySelector("#password").value;
-    const confirmPassword = document.querySelector("#confirm-password").value;
+    const userPassword = document.querySelector("#password");
+    const confirmPassword = document.querySelector("#confirm-password");
     passwordValidation(userPassword, confirmPassword);
-    //event.preventDefault();
+    event.preventDefault();
 });
 
 function passwordValidation(userPassword, confirmPassword) {
-    //const noMatch = document.querySelector("#no-match");
-    if (userPassword != confirmPassword || !confirmPassword) {
+    const noMatch = document.querySelector("#no-match");
+    if (userPassword.value != confirmPassword.value || !confirmPassword.value) {
         //let userPasswordFull = document.querySelector("#password");
         userPassword.className = "invalid-input";
         confirmPassword.className = "invalid-input";
-        //noMatch.setAttribute("style", "visibility: visible");
+        noMatch.setAttribute("style", "visibility: visible");
         console.log("bad password");
-    } //else {
-        //userPassword.className = "";
-        //confirmPassword.className = "";
-        //noMatch.setAttribute("style", "visibility: hidden");
-        //console.log("good password");
+    } else {
+        userPassword.className = "";
+        confirmPassword.className = "";
+        noMatch.setAttribute("style", "visibility: hidden");
+        console.log("good password");
     }
+}
